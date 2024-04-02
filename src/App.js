368b4e59ea7bdd407f6b3d3743fe45ebdad7db48
nodeSkin <shittujamiu@gmail.com>
2024-04-02 22:57:14 +0100

    exploring routes and route from RRD

--- src/App.js
+++ src/App.js
@@ -1,37 +1,20 @@
-import Diretory from "./components/directory/diretory.component";
+import Home from "./routes/home/home.component";
+import { Route, Routes } from "react-router-dom";
+
+//Routes allow the app to register the route level components that
+// will then render the component that matches the specific component
+
+const ShopRoute = () => {
+  return <h2>I AM SHOP COMP</h2>;
+};
 
 const App = () => {
-  const categories = [
-    {
-      id: 1,
-      title: "hats",
-      imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
-    },
-    {
-      id: 2,
-      title: "jackets",
-      imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
-    },
-    {
-      id: 3,
-      title: "sneakers",
-      imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
-    },
-    {
-      id: 4,
-      title: "womens",
-      imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
-    },
-    {
-      id: 5,
-      title: "mens",
-      imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
-    },
-  ];
   return (
-    <div>
-      <Diretory categories = {categories}/>
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />}>
+        <Route path="shop" element={<ShopRoute />} />
+      </Route>
+    </Routes>
   );
 };
 
---
+++ src/routes/home/home.component.jsx
@@ -0,0 +1,43 @@
+import { Outlet } from "react-router-dom";
+import Diretory from "../../components/directory/diretory.component";
+
+//Outlet allows us to leverage the pattern matching in the nesting structure 
+//in other to dynamically change portion of our codes based on the routes and nested route
+
+const Home = () => {
+  const categories = [
+    {
+      id: 1,
+      title: "hats",
+      imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
+    },
+    {
+      id: 2,
+      title: "jackets",
+      imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
+    },
+    {
+      id: 3,
+      title: "sneakers",
+      imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
+    },
+    {
+      id: 4,
+      title: "womens",
+      imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
+    },
+    {
+      id: 5,
+      title: "mens",
+      imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
+    },
+  ];
+  return (
+    <div>
+    <Diretory categories = {categories} />
+    <Outlet/>
+    </div>
+  );
+};
+
+export default Home;
